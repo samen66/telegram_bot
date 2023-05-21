d584e784167cf3fa73bb0bda11fb48fbfeb69c4c
SAmen <samenuatkhan@gmail.com>
2023-05-21 14:50:05 +0600

    TB-4: added migration v0001, added Service and Repository for Currency entity

---
+++ src/main/java/com/github/samen66/telegram_bot/service/CurrencyServiceImpl.java
@@ -0,0 +1,35 @@
+package com.github.samen66.telegram_bot.service;
+
+import com.github.samen66.telegram_bot.model.Currency;
+import com.github.samen66.telegram_bot.repository.CurrencyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CurrencyServiceImpl implements CurrencyService {
+    private final CurrencyRepository currencyRepository;
+    @Autowired
+    public CurrencyServiceImpl(CurrencyRepository currencyRepository) {
+        this.currencyRepository = currencyRepository;
+    }
+
+    @Override
+    public void save(Currency currency) {
+        currencyRepository.save(currency);
+    }
+
+    @Override
+    public List<Currency> getAllCurrency() {
+        return currencyRepository.findAll();
+    }
+
+    @Override
+    public Currency findById(Integer id) {
+        return currencyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Currency findByCode(String code) {
+        return currencyRepository.findFirstByCode(code);
+    }
+}
